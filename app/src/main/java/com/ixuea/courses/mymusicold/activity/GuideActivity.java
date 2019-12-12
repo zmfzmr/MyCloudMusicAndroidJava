966dfd4fa486dcf512bf1cdc561de170efd6ede5
zmfzmr <1554567593@qq.com>
2019-12-12 20:52:33 +0800

    3522 Add guide activity button click.

--- app/src/main/java/com/ixuea/courses/mymusicold/activity/GuideActivity.java
+++ app/src/main/java/com/ixuea/courses/mymusicold/activity/GuideActivity.java
@@ -3,13 +3,21 @@ package com.ixuea.courses.mymusicold.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.ixuea.courses.mymusicold.R;
 /**
  * 引导界面
  */
-public class GuideActivity extends AppCompatActivity {
+public class GuideActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "GuideActivity";
+    private Button bt_login_or_register;//登录注册
+    private Button bt_enter;
+
 
     /**
      * 当前界面创建的时候
@@ -28,5 +36,37 @@ public class GuideActivity extends AppCompatActivity {
         //隐藏状态栏
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //找控件
+        //登录注册按钮 (ctrl + alt + f创建实例变量（又局部变为全局变量）)
+        bt_login_or_register = findViewById(R.id.bt_login_or_register);
+        //立即体验按钮
+        bt_enter = findViewById(R.id.bt_enter);
+
+        //设置点击事件
+        bt_login_or_register.setOnClickListener(this);
+        bt_enter.setOnClickListener(this);
+
+    }
+
+    /**
+     *点击回调方法
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.bt_login_or_register:
+                //注册登录按钮点击
+                Log.d(TAG, "onClick login or register");
+                break;
+            case R.id.bt_enter:
+                //进入按钮
+                Log.d(TAG, "onClick enter");
+                break;
+            default:
+                break;
+        }
+
     }
 }
