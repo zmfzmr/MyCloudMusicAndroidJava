16b26c0023ebb4d910e9e750eeb6ab15869a402d
zmfzmr <1554567593@qq.com>
2019-12-25 12:16:58 +0800

    3563 Add tool bar.

--- app/src/main/java/com/ixuea/courses/mymusicold/activity/LoginActivity.java
+++ app/src/main/java/com/ixuea/courses/mymusicold/activity/LoginActivity.java
@@ -1,19 +1,41 @@
 package com.ixuea.courses.mymusicold.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import butterknife.BindView;
 
 import android.os.Bundle;
+
 
 import com.ixuea.courses.mymusicold.R;
 
 /**
  * 登录界面
  */
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseCommonActivity {
+
+    /**
+     * 标题控件
+     */
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    @Override
+    protected void initView() {
+        super.initView();
+
+        //初始化Toolbar
+        setSupportActionBar(toolbar);
+    }
+
+//    @Override
+//    public void setTitle(int titleId) {//这个方法可以不用写，因为已经继承了
+//        super.setTitle(titleId);
+//    }
 }
