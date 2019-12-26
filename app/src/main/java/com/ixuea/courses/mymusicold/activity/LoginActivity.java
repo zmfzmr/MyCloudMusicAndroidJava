0afbc6a5375590a268f6687a12db932f8e01dedf
zmfzmr <1554567593@qq.com>
2019-12-26 21:55:49 +0800

    3570 Add login activity click.

--- app/src/main/java/com/ixuea/courses/mymusicold/activity/LoginActivity.java
+++ app/src/main/java/com/ixuea/courses/mymusicold/activity/LoginActivity.java
@@ -3,8 +3,12 @@ package com.ixuea.courses.mymusicold.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import butterknife.BindView;
+import butterknife.OnClick;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 import com.ixuea.courses.mymusicold.R;
@@ -14,10 +18,36 @@ import com.ixuea.courses.mymusicold.R;
  */
 public class LoginActivity extends BaseTitleActivity {
 
+    private static final String TAG = "LoginActivity";
+    @BindView(R.id.et_username)//用户名输入框
+    EditText et_username;
+
+    @BindView(R.id.et_password)//密码输入框
+    EditText et_password;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    /**
+     * 登录按钮点击了
+     *
+     * 如果想要获取到这个按钮，可以直接传入参数Button view
+     */
+    @OnClick(R.id.bt_login)
+    public void onLoginClick(Button view) {
+        Log.d(TAG, "onLoginClick: ");
+
+    }
+
+    @OnClick(R.id.bt_forget_password)
+    public void onForgetPasswordClick(Button view) {
+        Log.d(TAG, "onForgetPasswordClick: ");
+
     }
 
 }
