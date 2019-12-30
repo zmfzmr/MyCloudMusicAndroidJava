604298b68203feb4648f354b74ab011767680083
zmfzmr <1554567593@qq.com>
2019-12-30 11:51:34 +0800

    3578 Add check phone format.

--- app/src/main/java/com/ixuea/courses/mymusicold/util/StringUtil.java
+++ app/src/main/java/com/ixuea/courses/mymusicold/util/StringUtil.java
@@ -1,5 +1,7 @@
 package com.ixuea.courses.mymusicold.util;
 
+import static com.ixuea.courses.mymusicold.util.Constant.REGEX_PHONE;
+
 /**
  * 字符串工具类
  * 注意：这个和StringUtils的区别，StringUtils是导入依赖commons-lang3 包中的类
@@ -13,7 +15,7 @@ public class StringUtil {
      * @return 返回是否符合手机号  默认返回false
      */
     public static boolean isPhone(String value) {
-        //TODO 实现手机号格式判断
-        return false;
+        //看传入的值和正则表达式，是否匹配的上，如果可以的话，就返回true，否则false
+        return value.matches(REGEX_PHONE);
     }
 }
