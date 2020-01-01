176e7362dd7b7c9bd13c217ea32ee5e262ea0494
zmfzmr <1554567593@qq.com>
2020-01-01 16:49:36 +0800

    4657 Add multi dex.和遗漏的2行代码（21行和27行）

--- app/src/main/java/com/ixuea/courses/mymusicold/AppContext.java
+++ app/src/main/java/com/ixuea/courses/mymusicold/AppContext.java
@@ -1,15 +1,30 @@
 package com.ixuea.courses.mymusicold;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.ixuea.courses.mymusicold.util.ToastUtil;
 
+import androidx.multidex.MultiDex;
 import es.dmoral.toasty.Toasty;
 
 /**
  * 全局Application
  */
 public class AppContext extends Application {
+//    public class AppContext extends MultiDexApplication {
+
+    /**
+     * 上下文
+     */
+    private static AppContext context;
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        //初始化MultiDex
+        MultiDex.install(this);
+    }
 
     /**
      * 创建了 （第一次创建应用或者杀掉应用后就会执行一次这个方法）
@@ -17,6 +32,8 @@ public class AppContext extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        context = this;
         //初始化toast工具类
         Toasty.Config.getInstance().apply();
         //初始化Toast工具类
