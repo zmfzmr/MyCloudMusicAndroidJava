f9feda5386262d5070bbaea193c441f485aaee12
zmfzmr <1554567593@qq.com>
2020-01-02 23:15:49 +0800

    3590 Add sheet detail api.（其实配置了一个Retrofit接口）

---
+++ app/src/main/java/com/ixuea/courses/mymusicold/api/Service.java
@@ -0,0 +1,35 @@
+package com.ixuea.courses.mymusicold.api;
+
+import com.ixuea.courses.mymusicold.domain.SheetDetailWrapper;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * 网络接口配置
+ * <p>
+ * 之所以调用接口还能返回数据
+ * 是因为Retrofit框架内部处理了
+ * 这里不讲解原理
+ * 在《详解Retrofit》课程中讲解
+ */
+public interface Service {
+
+    /**
+     * 歌单详情
+     *
+     * @param id {id} 这样表示id，表示的@Path("id")里面的id，
+     *           path里面的id其实就是接收后面String id 的值
+     *           <p>
+     *           一般情况下，三个名称都写成一样，比如3个都是id
+     *           <p>
+     *           //Retrofit如何知道我们传入的是id呢，其实通过Retrofit注解@Path("id")知道
+     *           (应该是相等于限定了id，其他的应该会报错)
+     *           <p>
+     *           Observable<SheetDetailWrapper>：相等于把json数据转换成这个SheetDetailWrapper类型的对象
+     *           Observable：rxjava里面的类
+     */
+    @GET("v1/sheets/{id}")
+    Observable<SheetDetailWrapper> sheetDetail(@Path("id") String id);
+}
