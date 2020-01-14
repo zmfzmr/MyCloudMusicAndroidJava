de3e0d5d560db3e91cf6210030e4920204d3b4e7
zmfzmr <1554567593@qq.com>
2020-01-15 00:31:28 +0800

    3665 Add send code count down.(验证码倒计时)

--- app/src/main/java/com/ixuea/courses/mymusic/activity/ForgetPasswordActivity.java
+++ app/src/main/java/com/ixuea/courses/mymusic/activity/ForgetPasswordActivity.java
@@ -1,6 +1,7 @@
 package com.ixuea.courses.mymusic.activity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -40,6 +41,7 @@ public class ForgetPasswordActivity extends BaseLoginActivity {
             EditText et_confirm_password;
     private String phone;//手机号
     private String email;//邮箱
+    private CountDownTimer countDownTimer;
 
 
     @Override
@@ -54,7 +56,10 @@ public class ForgetPasswordActivity extends BaseLoginActivity {
     @OnClick(R.id.bt_send_code)
     public void onSendCodeClick() {
         LogUtil.d(TAG, "onSendCodeClick");
+
+        startCountDown();
     }
+
 
     /**
      * 找回密码按钮点击了
@@ -148,5 +153,53 @@ public class ForgetPasswordActivity extends BaseLoginActivity {
                     }
                 });
 
+    }
+
+    /**
+     * 开始倒计时
+     * 现在没有保存退出的状态
+     * 也就说，返回在进来就可以点击了
+     */
+    private void startCountDown() {
+        //倒计时的总时间,间隔
+        //单位是毫秒
+        countDownTimer = new CountDownTimer(60000, 1000) {
+
+            /**
+             * 每间隔时间调用
+             * @param millisUntilFinished 这个相当于剩余结束的时间
+             */
+            @Override
+            public void onTick(long millisUntilFinished) {
+                //%1：表示第一个参数  $d：表示是数字   连在一起到时候显示为59 58 ..50 49 这种形式 -->
+                bt_send_code.setText(getString(R.string.count_second, millisUntilFinished / 1000));
+            }
+
+            /**
+             * 倒计时完成时
+             */
+
+            @Override
+            public void onFinish() {
+                //结束完成后恢复原来文字和开启按钮
+                bt_send_code.setText(R.string.send_code);
+                bt_send_code.setEnabled(true);
+            }
+        };
+        //启动
+        countDownTimer.start();//记得开启
+        //禁用按钮
+        bt_send_code.setEnabled(false);
+
+    }
+
+    @Override
+    protected void onDestroy() {//这步很重要，记得销毁，否则造成内存泄漏
+        //销毁定时器
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+            countDownTimer = null;
+        }
+        super.onDestroy();
     }
 }
