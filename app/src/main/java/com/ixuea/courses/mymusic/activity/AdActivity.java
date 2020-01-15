7705df51c97fa598cff55808f47fa6273332404f
zmfzmr <1554567593@qq.com>
2020-01-15 23:16:17 +0800

    4660 Add  ad activity layout.（广告布局)

--- app/src/main/java/com/ixuea/courses/mymusic/activity/AdActivity.java
+++ app/src/main/java/com/ixuea/courses/mymusic/activity/AdActivity.java
@@ -1,13 +1,26 @@
 package com.ixuea.courses.mymusic.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.ixuea.courses.mymusic.R;
+import com.ixuea.courses.mymusic.util.LogUtil;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * 广告界面
  */
 public class AdActivity extends BaseCommonActivity {
+
+    private static final String TAG = "AdActivity";
+
+    /**
+     * 跳过广告按钮
+     */
+    @BindView(R.id.bt_skip_ad)
+    Button bt_skip_ad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +34,21 @@ public class AdActivity extends BaseCommonActivity {
 
         //全屏
         fullScreen();
+    }
+
+    /**
+     * 广告点击了
+     */
+    @OnClick(R.id.bt_ad)
+    public void onAdClick() {
+        LogUtil.d(TAG, "onAdClick");
+    }
+
+    /**
+     * 跳过广告按钮
+     */
+    @OnClick(R.id.bt_skip_ad)
+    public void onSkipClick() {
+        LogUtil.d(TAG, "onSkipClick");
     }
 }
