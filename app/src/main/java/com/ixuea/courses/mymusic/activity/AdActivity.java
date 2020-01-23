69e0e7e8646e82f7541d13c062f90ad68c0c837c
zmfzmr <1554567593@qq.com>
2020-01-23 15:50:06 +0800

    4661 Add show ad.（广告倒计时和跳过（注意取消倒计时，防止内存泄漏）)

--- app/src/main/java/com/ixuea/courses/mymusic/activity/AdActivity.java
+++ app/src/main/java/com/ixuea/courses/mymusic/activity/AdActivity.java
@@ -1,8 +1,10 @@
 package com.ixuea.courses.mymusic.activity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.Button;
 
+import com.ixuea.courses.mymusic.MainActivity;
 import com.ixuea.courses.mymusic.R;
 import com.ixuea.courses.mymusic.util.LogUtil;
 
@@ -21,6 +23,7 @@ public class AdActivity extends BaseCommonActivity {
      */
     @BindView(R.id.bt_skip_ad)
     Button bt_skip_ad;
+    private CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +31,57 @@ public class AdActivity extends BaseCommonActivity {
         setContentView(R.layout.activity_ad);
     }
 
+
     @Override
     protected void initView() {
         super.initView();
 
         //全屏
         fullScreen();
+    }
+
+    /**
+     * 界面以及显示出来了调用
+     * 从后台切换到前台还会调用
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //倒计时的总时间,间隔
+        //单位为毫秒
+
+        //创建倒计时
+        countDownTimer = new CountDownTimer(5000, 1000) {
+
+            /**
+             * 每次间隔调用
+             */
+            @Override
+            public void onTick(long millisUntilFinished) {
+                //可能会倒回到0，所以这里加1
+                bt_skip_ad.setText(getString(R.string.count_second, millisUntilFinished / 1000 + 1));
+            }
+
+            /**
+             * 倒计时完成
+             */
+            @Override
+            public void onFinish() {
+                //执行下一步方法
+                next();
+            }
+        };
+
+        //启动定时器
+        countDownTimer.start();
+    }
+
+    /**
+     * 进入首页
+     */
+    private void next() {
+        startActivityAfterFinishThis(MainActivity.class);
     }
 
     /**
@@ -50,5 +98,26 @@ public class AdActivity extends BaseCommonActivity {
     @OnClick(R.id.bt_skip_ad)
     public void onSkipClick() {
         LogUtil.d(TAG, "onSkipClick");
+        //取消倒计时并跳转到首页
+        cancelCountDown();//记得取消倒计时，否则会出现内存泄漏
+        next();
+    }
+
+    /**
+     * 界面销毁了调用
+     */
+    @Override
+    protected void onDestroy() {
+        cancelCountDown();
+        super.onDestroy();
+    }
+
+    /**
+     * 取消倒计时
+     */
+    private void cancelCountDown() {
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
     }
 }
