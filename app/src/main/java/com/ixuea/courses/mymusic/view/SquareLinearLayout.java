7cd97378ed44423edaa2a9fa2fc4f10a03e9bbfc
zmfzmr <1554567593@qq.com>
2020-02-11 18:09:04 +0800

    3728 Add sheet layout.

--- app/src/main/java/com/ixuea/courses/mymusic/view/SquareLinearLayout.java
+++ app/src/main/java/com/ixuea/courses/mymusic/view/SquareLinearLayout.java
@@ -40,12 +40,15 @@ public class SquareLinearLayout extends LinearLayout {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         //设置系统测量好的尺寸
+        //这里是根据父类里的规则存储测量到的宽高
+        // （如果没有调用setMeasuredDimension这个方法，那么下面getMeasuredWidth应该是为null的）
+        //也可以用第二种方式（把super.onMeasure()放在前面，后面再获取宽高的话，应该没有问题）
         setMeasuredDimension(getDefaultSize(0, widthMeasureSpec), getDefaultSize(0, heightMeasureSpec));
 
         //创建一个测量规则
         int width = getMeasuredWidth();
         widthMeasureSpec = heightMeasureSpec = MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY);
-        //让父类测量规则
+        //让父类设置尺寸
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 }
