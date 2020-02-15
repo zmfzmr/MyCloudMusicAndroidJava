33508351537d56504f8a85025df39dc5b78b22f0
zmfzmr <1554567593@qq.com>
2020-02-15 17:15:47 +0800

    3749 Add show sheet detail song.（显示歌单歌曲数据）

--- app/src/main/java/com/ixuea/courses/mymusic/adapter/SongAdapter.java
+++ app/src/main/java/com/ixuea/courses/mymusic/adapter/SongAdapter.java
@@ -2,6 +2,7 @@ package com.ixuea.courses.mymusic.adapter;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
+import com.ixuea.courses.mymusic.R;
 import com.ixuea.courses.mymusic.domain.Song;
 
 import androidx.annotation.NonNull;
@@ -23,10 +24,16 @@ public class SongAdapter extends BaseQuickAdapter<Song, BaseViewHolder> {
      * 显示数据
      *
      * @param helper BaseViewHolder
-     * @param item   Song
+     * @param data Song 因为继承泛型的时候已经传入了Song类型，所以这里不需要向下转型了
      */
     @Override
-    protected void convert(@NonNull BaseViewHolder helper, Song item) {
+    protected void convert(@NonNull BaseViewHolder helper, Song data) {
+        //显示位置 helper.getAdapterPosition():获取集合数据的索引（所以从0开始，所以需要+1）
+        helper.setText(R.id.tv_position, String.valueOf(helper.getAdapterPosition() + 1));
+        //显示标题
+        helper.setText(R.id.tv_title, data.getTitle());
+        //显示信息(歌曲名称)
+        helper.setText(R.id.tv_info, data.getSinger().getNickname());
 
     }
 }
