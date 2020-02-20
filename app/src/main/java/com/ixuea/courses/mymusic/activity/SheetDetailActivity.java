8e339b8af2868d24906325931393db914b3ca612
zmfzmr <1554567593@qq.com>
2020-02-20 17:41:14 +0800

    3753 Add show sheet detail header data.

--- app/src/main/java/com/ixuea/courses/mymusic/activity/SheetDetailActivity.java
+++ app/src/main/java/com/ixuea/courses/mymusic/activity/SheetDetailActivity.java
@@ -3,6 +3,10 @@ package com.ixuea.courses.mymusic.activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.ixuea.courses.mymusic.R;
 import com.ixuea.courses.mymusic.adapter.SongAdapter;
@@ -11,7 +15,10 @@ import com.ixuea.courses.mymusic.domain.Sheet;
 import com.ixuea.courses.mymusic.domain.response.DetailResponse;
 import com.ixuea.courses.mymusic.listener.HttpObserver;
 import com.ixuea.courses.mymusic.util.Constant;
+import com.ixuea.courses.mymusic.util.ImageUtil;
 import com.ixuea.courses.mymusic.util.LogUtil;
+
+import org.apache.commons.lang3.StringUtils;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -36,6 +43,19 @@ public class SheetDetailActivity extends BaseTitleActivity {
     private String id;
     private Sheet data;//歌单对象数据
     private SongAdapter adapter;
+    /**
+     * 头部容器
+     */
+    private LinearLayout ll_header;//这里也可以命名为LinearLayout，也可以不命名
+    private ImageView iv_banner;//封面图
+    private TextView tv_title;//歌单详情头部标题（也就是歌单的名称）
+    private ImageView iv_avatar;//头像
+    private TextView tv_nickname;//头像
+    private View ll_comment_container;//评论容器
+    private TextView tv_comment_count;//评论数
+    private Button bt_collection;//收藏按钮
+    private View ll_play_all_container;//播放全部容器
+    private TextView tv_count;//歌曲数
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,6 +89,36 @@ public class SheetDetailActivity extends BaseTitleActivity {
     private View createHeaderView() {
         //从XML创建布局
         View view = getLayoutInflater().inflate(R.layout.header_sheet_detail, (ViewGroup) rv.getParent(), false);
+
+        //头部容器
+        ll_header = view.findViewById(R.id.ll_header);
+
+        //封面图
+        iv_banner = view.findViewById(R.id.iv_banner);
+
+        //标题
+        tv_title = view.findViewById(R.id.tv_title);
+
+        //头像
+        iv_avatar = view.findViewById(R.id.iv_avatar);
+
+        //昵称
+        tv_nickname = view.findViewById(R.id.tv_nickname);
+
+        //评论容器
+        ll_comment_container = view.findViewById(R.id.ll_comment_container);
+
+        //评论数据
+        tv_comment_count = view.findViewById(R.id.tv_comment_count);
+
+        //收藏按钮
+        bt_collection = view.findViewById(R.id.bt_collection);
+
+        //播放全部容器
+        ll_play_all_container = view.findViewById(R.id.ll_play_all_container);
+
+        //歌曲数
+        tv_count = view.findViewById(R.id.tv_count);
 
         //返回view
         return view;
@@ -126,5 +176,31 @@ public class SheetDetailActivity extends BaseTitleActivity {
             //设置数据
             adapter.replaceData(data.getSongs());
         }
+
+        //显示封面
+        if (StringUtils.isBlank(data.getBanner())) {
+            //如果图片为空
+
+            //就用默认图片
+            iv_banner.setImageResource(R.drawable.placeholder);
+        } else {
+            //有图片
+            ImageUtil.show(getMainActivity(), iv_banner, data.getBanner());
+        }
+
+        //显示标题
+        tv_title.setText(data.getTitle());
+
+        //头像
+        ImageUtil.showAvatar(getMainActivity(), iv_avatar, data.getUser().getAvatar());
+
+        //昵称
+        tv_nickname.setText(data.getUser().getNickname());
+
+        //评论数
+        tv_comment_count.setText(String.valueOf(data.getComments_count()));
+
+        //音乐数
+        tv_count.setText(getResources().getString(R.string.music_count, data.getSongs().size()));
     }
 }
