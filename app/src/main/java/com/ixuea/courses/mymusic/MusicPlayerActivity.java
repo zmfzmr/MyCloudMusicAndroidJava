b15e941e59e422ca76069d1c4945a91d9b50ad8a
zmfzmr <1554567593@qq.com>
2020-03-15 13:07:42 +0800

    3880 Add music play activity basic layout.(背景显示在状态栏;内容在状态栏外（用fitsSystemWindows属性）)

--- app/src/main/java/com/ixuea/courses/mymusic/MusicPlayerActivity.java
+++ app/src/main/java/com/ixuea/courses/mymusic/MusicPlayerActivity.java
@@ -4,17 +4,25 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.ixuea.courses.mymusic.activity.BaseTitleActivity;
 
 /**
  * 黑胶唱片界面
  */
-public class MusicPlayerActivity extends AppCompatActivity {
+public class MusicPlayerActivity extends BaseTitleActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_player);
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+
+        //显示亮色状态栏(父类的这个方法里面，已经把toolbar文字设置为白色了和内容显示到状态栏)
+        lightStatusBar();
     }
 
     /**
