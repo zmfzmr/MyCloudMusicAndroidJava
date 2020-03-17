bf6906a0e4b51cf2011c5e2db43032031e9a77f8
zmfzmr <1554567593@qq.com>
2020-03-17 16:36:36 +0800

    3876 Add show music player activity background.(高斯模糊)

--- app/src/main/java/com/ixuea/courses/mymusic/MusicPlayerActivity.java
+++ app/src/main/java/com/ixuea/courses/mymusic/MusicPlayerActivity.java
@@ -2,17 +2,29 @@ package com.ixuea.courses.mymusic;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.ixuea.courses.mymusic.activity.BaseTitleActivity;
 import com.ixuea.courses.mymusic.domain.Song;
 import com.ixuea.courses.mymusic.manager.ListManager;
 import com.ixuea.courses.mymusic.manager.MusicPlayerManager;
 import com.ixuea.courses.mymusic.service.MusicPlayerService;
-import com.ixuea.courses.mymusic.util.ImageUtil;
+import com.ixuea.courses.mymusic.util.ResourceUtil;
 
+import org.apache.commons.lang3.StringUtils;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import butterknife.BindView;
+import jp.wasabeef.glide.transformations.BlurTransformation;
+
 
 /**
  * 黑胶唱片界面
@@ -78,8 +90,50 @@ public class MusicPlayerActivity extends BaseTitleActivity {
         //设置子标题(用toolbar设置)
         toolbar.setSubtitle(data.getSinger().getNickname());
 
-        //显示背景
-        ImageUtil.show(getMainActivity(), iv_background, data.getBanner());
+//        //显示背景
+//        ImageUtil.show(getMainActivity(), iv_background, data.getBanner());
+
+        //实现背景高斯模糊效果 返回的是RequestBuilder<Drawable>
+        RequestBuilder<Drawable> requestBuilder = Glide.with(this).asDrawable();
+
+        if (StringUtils.isBlank(data.getBanner())) {//data:正在播放的音乐
+            //没有封面图
+
+            //使用默认封面图
+            requestBuilder.load(R.drawable.default_album);
+        } else {
+            //使用真是图片
+            requestBuilder.load(ResourceUtil.resourceUri(data.getBanner()));
+        }
+        //创建请求选项
+        //传入了BlurTransformation
+        //用来实现高斯模糊
+        //radius:模糊半径；值越大越模糊
+        //sampling:采样率；值越大越模糊
+
+        //bitmapTransform方法是Glide中的RequestOptions类中的
+        //BlurTransformation:是新添加的图片变换框架里面的 blur：模拟
+        RequestOptions options = RequestOptions.bitmapTransform(new BlurTransformation(25, 3));
+
+        //加载图片
+        requestBuilder
+                .apply(options)
+//                .into(iv_background);//后面需要实现一些效果，所以先不用这个
+                .into(new CustomTarget<Drawable>() {
+                    /**
+                     * 资源下载成功
+                     */
+                    @Override
+                    public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                        //设置到背景控件上
+                        iv_background.setImageDrawable(resource);
+                    }
+
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                    }
+                });
 
     }
     /**
