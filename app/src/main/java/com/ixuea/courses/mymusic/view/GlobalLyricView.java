42e0704f06e9c454e262d72703d9591e8d3febdc
zmfzmr <1554567593@qq.com>
2020-04-04 21:52:52 +0800

    3955 Add bind global lyric view.

--- app/src/main/java/com/ixuea/courses/mymusic/view/GlobalLyricView.java
+++ app/src/main/java/com/ixuea/courses/mymusic/view/GlobalLyricView.java
@@ -5,17 +5,57 @@ import android.os.Build;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.ixuea.courses.mymusic.R;
+import com.ixuea.courses.mymusic.util.LogUtil;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * 全局(桌面)歌词
  */
 public class GlobalLyricView extends LinearLayout {
+    private static final String TAG = "GlobalLyricView";
+    /**
+     * 第一行歌词控件
+     */
+    @BindView(R.id.llv1)
+    LyricLineView llv1;
+
+    /**
+     * 第二行歌词控件
+     */
+    @BindView(R.id.llv2)
+    LyricLineView llv2;
+    /**
+     * logo
+     */
+    @BindView(R.id.iv_logo)
+    ImageView iv_logo;
+
+    /**
+     * 关闭按钮
+     */
+    @BindView(R.id.iv_close)
+    ImageView iv_close;
+    /**
+     * 播放控制容器
+     */
+    @BindView(R.id.ll_play_container)
+    View ll_play_all_container;
+
+    /**
+     * 播放按钮
+     */
+    @BindView(R.id.iv_play)
+    ImageView iv_play;
+
     /**
      * 构造方法
      */
@@ -62,6 +102,9 @@ public class GlobalLyricView extends LinearLayout {
         //添加到当前容器
         addView(view);
         //这样的话，外界在使用这个控件(GlobalLyricView)的时候,就可以使用布局item_global_lyric的内容啦
+
+        //初始化View注入框架
+        ButterKnife.bind(this);
     }
 
     /**
@@ -69,6 +112,62 @@ public class GlobalLyricView extends LinearLayout {
      */
     private void setBackground() {
         setBackgroundColor(getResources().getColor(R.color.global_lyric_background));
+    }
+
+    /**
+     * logo点击
+     */
+    @OnClick(R.id.iv_logo)
+    public void onLogoClick() {
+        LogUtil.d(TAG, "onLogoClick");
+    }
+
+    /**
+     * 关闭点击
+     */
+    @OnClick(R.id.iv_close)
+    public void onCloseClick() {
+        LogUtil.d(TAG, "onCloseClick");
+    }
+
+    /**
+     * 锁定点击
+     */
+    @OnClick(R.id.iv_lock)
+    public void onLockClick() {
+        LogUtil.d(TAG, "onLockClick");
+    }
+
+    /**
+     * 上一曲
+     */
+    @OnClick(R.id.iv_previous)
+    public void onPreviousClick() {
+        LogUtil.d(TAG, "onPreviousClick");
+    }
+
+    /**
+     * 播放点击
+     */
+    @OnClick(R.id.iv_play)
+    public void onPlayClick() {
+        LogUtil.d(TAG, "onPlayClick:");
+    }
+
+    /**
+     * 下一曲
+     */
+    @OnClick(R.id.iv_next)
+    public void onNextClick() {
+        LogUtil.d(TAG, "onNextClick:");
+    }
+
+    /**
+     * 设置点击
+     */
+    @OnClick(R.id.iv_settings)
+    public void onSettingsClick() {
+        LogUtil.d(TAG, "onSettingsClick:");
     }
 
 }
