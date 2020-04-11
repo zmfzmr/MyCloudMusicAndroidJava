c5fedf6ab2c7a50596f2b5ecd6b9c1dbd8f02544
zmfzmr <1554567593@qq.com>
2020-04-11 22:40:28 +0800

    3982 Add image scale. (1.添加依赖com.github.chrisbanes:PhotoView 2.在activity中在PhotoView显示图片)

--- app/src/main/java/com/ixuea/courses/mymusic/activity/ImagePreviewActivity.java
+++ app/src/main/java/com/ixuea/courses/mymusic/activity/ImagePreviewActivity.java
@@ -4,21 +4,53 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.github.chrisbanes.photoview.PhotoView;
 import com.ixuea.courses.mymusic.R;
 import com.ixuea.courses.mymusic.util.Constant;
+import com.ixuea.courses.mymusic.util.ImageUtil;
 
-import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
 
 /**
  * 图片预览界面
  * 可以把该界面实现为项目中通用的图片预览界面
  */
-public class ImagePreviewActivity extends AppCompatActivity {
+public class ImagePreviewActivity extends BaseCommonActivity {
+    /**
+     * 找到添加的依赖 PhotoView控件
+     */
+    @BindView(R.id.pv)
+    PhotoView pv;
+    private String id;//这个是传递过来的Song id，目前还没有用到
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_preview);
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+
+        //亮色状态栏
+        lightStatusBar();
+    }
+
+    //亮色状态栏
+
+    @Override
+    protected void initDatum() {
+        super.initDatum();
+
+        //获取id
+        id = extraId();
+        //获取图片地址
+        url = extraString(Constant.URL);
+        //显示图片
+        ImageUtil.show(getMainActivity(), pv, url);
+
     }
 
     /**
