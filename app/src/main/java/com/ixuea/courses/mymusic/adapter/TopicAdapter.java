d59c008fecd0352b1367892aa5f5d6224cfdbe66
zmfzmr <1554567593@qq.com>
2020-04-21 20:19:38 +0800

    4042 Add show topic data.

--- app/src/main/java/com/ixuea/courses/mymusic/adapter/TopicAdapter.java
+++ app/src/main/java/com/ixuea/courses/mymusic/adapter/TopicAdapter.java
@@ -1,8 +1,13 @@
 package com.ixuea.courses.mymusic.adapter;
 
+import android.app.Activity;
+import android.widget.ImageView;
+
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
+import com.ixuea.courses.mymusic.R;
 import com.ixuea.courses.mymusic.domain.Topic;
+import com.ixuea.courses.mymusic.util.ImageUtil;
 
 import androidx.annotation.NonNull;
 
@@ -20,7 +25,16 @@ public class TopicAdapter extends BaseQuickAdapter<Topic, BaseViewHolder> {
     }
 
     @Override
-    protected void convert(@NonNull BaseViewHolder helper, Topic item) {
+    protected void convert(@NonNull BaseViewHolder helper, Topic data) {
+        //helper:翻译：帮手
+        ImageView iv_banner = helper.getView(R.id.iv_banner);
 
+        //显示封面
+        ImageUtil.show((Activity) mContext, iv_banner, data.getBanner());
+        //显示标题
+        helper.setText(R.id.tv_title, "#" + data.getTitle() + "#");
+        //显示参与人数
+        helper.setText(R.id.tv_info,
+                mContext.getResources().getString(R.string.join_count, data.getJoins_count()));
     }
 }
