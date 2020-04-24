dd786b3a5eaf00c0e78baf570275e7a333e1aa71
zmfzmr <1554567593@qq.com>
2020-04-24 18:02:15 +0800

    4058 Add search config in tool bar.(在onCreateOptionsMenu方法里面写，记得监听返回true)

--- app/src/main/java/com/ixuea/courses/mymusic/activity/SelectFriendActivity.java
+++ app/src/main/java/com/ixuea/courses/mymusic/activity/SelectFriendActivity.java
@@ -1,6 +1,8 @@
 package com.ixuea.courses.mymusic.activity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.ixuea.courses.mymusic.R;
 import com.ixuea.courses.mymusic.adapter.FriendAdapter;
@@ -10,9 +12,11 @@ import com.ixuea.courses.mymusic.domain.UserResult;
 import com.ixuea.courses.mymusic.domain.response.ListResponse;
 import com.ixuea.courses.mymusic.listener.HttpObserver;
 import com.ixuea.courses.mymusic.util.DataUtil;
+import com.ixuea.courses.mymusic.util.LogUtil;
 
 import java.util.List;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,6 +26,7 @@ import butterknife.BindView;
  * 选择好友界面
  */
 public class SelectFriendActivity extends BaseTitleActivity {
+    private String TAG = "SelectFriendActivity";
     /**
      * 列表控件
      */
@@ -117,5 +122,84 @@ public class SelectFriendActivity extends BaseTitleActivity {
 
         //设置到适配器
         adapter.setDatum(userResult.getDatum());
+    }
+
+    /**
+     * 创建菜单方法
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //返回men布局
+        getMenuInflater().inflate(R.menu.search, menu);
+        //找到搜索按钮
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+
+        //这里找到的menu 布局里面的那个app:actionViewClass="androidx.appcompat.widget.SearchView" 类
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        //在这里就可以配置SearchView
+
+        //搜索监听器
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            /**
+             * 提交了搜索
+             *
+             * 在输入框中按回车
+             * 点击了键盘上的搜索按钮
+             * @param query
+             * @return
+             */
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                LogUtil.d(TAG, "onQueryTextSubmit");
+                onSearchTextChanged(query);
+
+                return true;//true表示处理了事件
+            }
+
+            /**
+             * 搜索框内容改变了(键盘点击搜索按钮的时候也会调用这个方法)
+             * @param newText
+             * @return
+             */
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                LogUtil.d(TAG, "onQueryTextChange");
+                onSearchTextChanged(newText);
+                return true;////true表示处理了事件
+            }
+        });
+
+        //是否进入界面就打开搜索栏，false为默认打开，默认为true
+        searchView.setIconified(false);
+        //搜索提示
+        searchView.setQueryHint(getString(R.string.hint_search_user));
+
+
+        //在搜索按钮里面查询搜索控件
+        return true;//这里设置返回true
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_search) {
+            //如果不需要特殊处理
+            //直接返回true就行了
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * 搜索我的好友
+     * 一般情况下好友数量比较少
+     * 可以直接在本地过滤
+     * 所以我们这里就讲解本地过滤
+     * 如果是网络搜索
+     * 那其实也是调用一个接口而已
+     * 所以也没多大难度
+     */
+    private void onSearchTextChanged(String query) {
+        LogUtil.d(TAG, "onSearchTextChanged");
     }
 }
