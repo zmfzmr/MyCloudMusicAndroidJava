acbcc4d6ea6367f933a8c4681448fc4fa060c9a4
zmfzmr <1554567593@qq.com>
2020-05-14 19:20:04 +0800

    4194 Add user about layout.(显示用户关于布局)

--- app/src/main/java/com/ixuea/courses/mymusic/fragment/UserDetailAboutFragment.java
+++ app/src/main/java/com/ixuea/courses/mymusic/fragment/UserDetailAboutFragment.java
@@ -4,14 +4,48 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.ixuea.courses.mymusic.R;
 import com.ixuea.courses.mymusic.util.Constant;
+
+import butterknife.BindView;
 
 /**
  * 用户详情-关于界面
  */
 public class UserDetailAboutFragment extends BaseCommonFragment {
+
+    /**
+     * 昵称
+     */
+    @BindView(R.id.tv_nickname)
+    TextView tv_nickname;
+
+    /**
+     * 性别
+     */
+    @BindView(R.id.tv_gender)
+    TextView tv_gender;
+
+    /**
+     * 生日
+     */
+    @BindView(R.id.tv_birthday)
+    TextView tv_birthday;
+
+    /**
+     * 地区
+     */
+    @BindView(R.id.tv_area)
+    TextView tv_area;
+
+    /**
+     * 描述
+     */
+    @BindView(R.id.tv_description)
+    TextView tv_description;
+
     /**
      * 返回显示的布局
      */
