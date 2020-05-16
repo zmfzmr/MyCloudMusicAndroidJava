2fff7dc0531bb50fc6159937565c8e362a8e453f
zmfzmr <1554567593@qq.com>
2020-05-16 22:26:28 +0800

    4214 Add video play or pause.(点击视频播放或暂停状态)

--- app/src/main/java/com/ixuea/courses/mymusic/activity/VideoDetailActivity.java
+++ app/src/main/java/com/ixuea/courses/mymusic/activity/VideoDetailActivity.java
@@ -19,6 +19,7 @@ import com.ixuea.courses.mymusic.util.LogUtil;
 import com.ixuea.courses.mymusic.util.ResourceUtil;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * 视频详情
@@ -128,5 +129,75 @@ public class VideoDetailActivity extends BaseTitleActivity {
 
         //标题(setTitle: 是Activity里面的方法，设置标题栏标题)
         setTitle(data.getTitle());
+    }
+
+    /**
+     * 播放按钮点击
+     */
+    @OnClick(R.id.ib_play)
+    public void onPlayClick() {
+        if (isPlaying()) {
+            //在播放
+
+            //暂停
+            pause();
+            LogUtil.d(TAG, "onPlayClick pause");
+        } else {
+            //没有播放
+
+            //播放
+            resume();
+        }
+    }
+
+    /**
+     * 继续播放(继续播放是 是一个暂停的图标)
+     */
+    private void resume() {
+        vv.start();
+
+        //显示暂停状态
+        showPauseStatus();
+    }
+
+    /**
+     * 暂停播放(是一个播放的图标)
+     */
+    private void pause() {
+        vv.pause();
+
+        //显示播放状态
+        showPlayStatus();
+    }
+
+    /**
+     * 是否是播放
+     */
+    private boolean isPlaying() {
+        //返回true，表示在播放
+        return vv.isPlaying();
+    }
+
+    /**
+     * 显示播放状态(这个是和Ui相关的)  播放图标
+     */
+    private void showPlayStatus() {
+        ib_play.setImageResource(R.drawable.ic_video_play);
+    }
+
+    /**
+     * 显示暂停状态(暂停图标)
+     */
+    private void showPauseStatus() {
+        ib_play.setImageResource(R.drawable.ic_video_pause);
+    }
+
+    @Override
+    protected void onDestroy() {
+        //释放播放器内部资源
+        //不然可能会有内存泄漏
+        vv.stopPlayback();
+
+        super.onDestroy();
     }
 }
