821c6a7b1a1149264124799abf7812bc67013139
zmfzmr <1554567593@qq.com>
2020-05-21 12:07:14 +0800

    4248 Add dynamic add image in recycler view.(1.动态添加图片RecyclerView 目前还没有数据(只是在FeedAdapter中进行了初始化操作(有数据就显示，否则隐藏)) 注意：1.还需要判断data.getImages() != null ，很重要，否则空指针异常 2.item 里面的正方形，用到了前面自定义的SquareLinearLayout)

--- app/src/main/java/com/ixuea/courses/mymusic/adapter/FeedAdapter.java
+++ app/src/main/java/com/ixuea/courses/mymusic/adapter/FeedAdapter.java
@@ -1,5 +1,6 @@
 package com.ixuea.courses.mymusic.adapter;
 
+import android.view.View;
 import android.widget.ImageView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -12,6 +13,7 @@ import com.ixuea.courses.mymusic.util.TimeUtil;
 import org.apache.commons.lang3.StringUtils;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * 动态适配器
@@ -53,5 +55,28 @@ public class FeedAdapter extends BaseQuickAdapter<Feed, BaseViewHolder> {
         //动态内容
 //        helper.setText(R.id.tv_content,data.getContent());
         helper.setText(R.id.tv_content, sb.toString());
+
+        //获取列表控件
+        RecyclerView rv = helper.getView(R.id.rv);
+
+        if (data.getImages() != null && data.getImages().size() > 0) {
+            //有图片
+
+            //显示图片列表控件
+            rv.setVisibility(View.VISIBLE);
+
+            //尺寸固定(当然这个也可以放到if外面)
+            rv.setHasFixedSize(true);
+
+            //禁用嵌套滚动（不希望内容多了后，里面的内容滚动(禁用内容滚动)）
+            //注意：传入的是false
+            rv.setNestedScrollingEnabled(false);
+        } else {
+            //没有图片
+
+            //隐藏控件
+            rv.setVisibility(View.GONE);
+        }
+
     }
 }
