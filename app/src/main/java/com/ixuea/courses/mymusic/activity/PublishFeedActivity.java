ec3e311721e963fa4b5353e9d5492d38b700a530
zmfzmr <1554567593@qq.com>
2020-05-22 22:58:34 +0800

    4256 Add feed layout.(发布动态布局(基本布局,还没完整,中间用的是一个占位控件))

--- app/src/main/java/com/ixuea/courses/mymusic/activity/PublishFeedActivity.java
+++ app/src/main/java/com/ixuea/courses/mymusic/activity/PublishFeedActivity.java
@@ -3,9 +3,14 @@ package com.ixuea.courses.mymusic.activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.ixuea.courses.mymusic.R;
 import com.ixuea.courses.mymusic.util.LogUtil;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * 发布动态界面
@@ -13,6 +18,24 @@ import com.ixuea.courses.mymusic.util.LogUtil;
 public class PublishFeedActivity extends BaseTitleActivity {
 
     private static final String TAG = "PublishFeedActivity";
+
+    /**
+     * 输入框
+     */
+    @BindView(R.id.et_content)
+    EditText et_content;
+
+    /**
+     * 当前位置
+     */
+    @BindView(R.id.tv_position)
+    TextView tv_position;
+
+    /**
+     * 字数统计
+     */
+    @BindView(R.id.tv_count)
+    TextView tv_count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,5 +68,13 @@ public class PublishFeedActivity extends BaseTitleActivity {
      */
     private void onSendMessageClick() {
         LogUtil.d(TAG, "onSendMessageClick");
+    }
+
+    /**
+     * 选择图片快捷按钮点击
+     */
+    @OnClick(R.id.ib_select_image)
+    public void onSelectImageClick() {
+        LogUtil.d(TAG, "onSelectImageClick");
     }
 }
