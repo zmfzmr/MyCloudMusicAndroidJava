a3dbe9c2ccd0421c3cef00590f196f2c62045163
zmfzmr <1554567593@qq.com>
2020-07-11 17:19:32 +0800

    4495 Add jiguang jmessage sdk init.(集成极光聊天 sdk)

---
+++ app/src/main/java/com/ixuea/courses/mymusic/service/JiGuangService.java
@@ -0,0 +1,12 @@
+package com.ixuea.courses.mymusic.service;
+
+import cn.jpush.android.service.JCommonService;
+
+/**
+ * JCore2.0.0及以上版本需要添加这个service
+ * 这个Service要继承JCommonService
+ * <p>
+ * 这里类不用实现了，因为都在父类实现了，这里继承而已，只是用这个名字JiGuangService
+ */
+public class JiGuangService extends JCommonService {
+}
