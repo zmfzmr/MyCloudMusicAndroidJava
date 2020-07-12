6356d329a5896542a5c43b163b9045d8102a6237
zmfzmr <1554567593@qq.com>
2020-07-12 17:17:30 +0800

    4500 Add test jmessage sdk is success.(测试极光sdk是否成功(test1和test2互为好友，test1给2发送消息 ，2能接受到消息))

--- app/src/main/java/com/ixuea/courses/mymusic/activity/Chat2Activity.java
+++ app/src/main/java/com/ixuea/courses/mymusic/activity/Chat2Activity.java
@@ -7,15 +7,37 @@ import android.os.Bundle;
 import com.ixuea.courses.mymusic.R;
 import com.ixuea.courses.mymusic.util.Constant;
 
+import cn.jpush.im.android.api.JMessageClient;
+import cn.jpush.im.android.api.model.Conversation;
+import cn.jpush.im.android.api.model.Message;
+
 /**
  * 聊天界面
  */
 public class Chat2Activity extends BaseTitleActivity {
 
+    private String id;//对方id
+    private Conversation conversation;//会话
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat2);
+    }
+
+    @Override
+    protected void initDatum() {
+        super.initDatum();
+        //获取id(对方id) 因为是从对方的用户详情 点击 (发送消息过来的)，传递的是对方的用户id
+        id = extraId();
+        //测试聊天SDK是否集成成功
+        //这里的用户必须是通过客户端注册的
+        //也就是要调用了极光IM的注册方法
+        conversation = Conversation.createSingleConversation(id);
+        //创建文本消息
+        Message message = conversation.createSendTextMessage("我们是爱学啊,这是一条文本消息");
+        //发送文本消息
+        JMessageClient.sendMessage(message);
     }
 
     /**
